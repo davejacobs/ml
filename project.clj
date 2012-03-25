29a8393f7280296e422b9a371bbb594cf46f8065
David Jacobs <david@wit.io>
2012-03-25 12:46:16 -0700

    upgrade to leiningen 2

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject ml "0.0.1-SNAPSHOT"
+(defproject ml/ml "0.0.1-SNAPSHOT" 
   :description "A study in machine learning, using Clojure"
-  :jvm-opts ["-D java.awt.headless=true"]
-  :repositories {"sonatype-oss-public"
-                 "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [incanter "1.3.0-SNAPSHOT"]]
-  :test-path "spec")
+  :repositories {"sonatype-oss-public"
+                 "https://oss.sonatype.org/content/groups/public/"}
+  :test-paths ["spec"]
+  :min-lein-version "2.0.0"
+  :jvm-opts ["-D java.awt.headless=true"])
