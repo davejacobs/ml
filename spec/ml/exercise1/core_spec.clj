81898f176b0d51781099bb1823cb52be4033aa35
David Jacobs <david@wit.io>
2012-03-25 01:09:54 -0700

    add more (almost) passing tests

--- spec/ml/exercise1/core_spec.clj
+++ spec/ml/exercise1/core_spec.clj
@@ -11,10 +11,22 @@
 
 (def y [10 20 10])
 
+(def theta (matrix [[0] [0] [0]]))
+
 (def expected-normalized-X
   (matrix [[1.0 -1.0  1.0]
            [1.0  0.0  0.0]
            [1.0  1.0 -1.0]]))
+
+(def expected-next-theta
+  (matrix [[0.1333]
+           [0.2667]
+           [0.2667]]))
+
+(def expected-final-theta
+  (matrix [[1.4814]
+           [2.9627]
+           [2.9627]]))
 
 (deftest normalize-one-value
   (testing "properly scales one value via mean and standard deviation"
@@ -38,3 +50,13 @@
     (let [exact-cost (cost X y [[0] [0] [0]])
           rounded-cost (round exact-cost)] 
       (is (= rounded-cost 100)))))
+
+(deftest calculate-next-theta
+  (testing "calculates the next set of thetas based on gradient descent"
+    (is (= (next-theta X y theta 0.01) expected-next-theta))))
+
+(deftest minimize-theta
+  (testing "minimizes theta over the iterations given"
+    (let [res (gradient-descent X y theta 0.01 100)
+          {final-theta :theta history :history} res]
+      (is (= final-theta expected-final-theta)))))
