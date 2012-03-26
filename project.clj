3f11af0ef66b786c91d8ff4d1007d248409bea71
David Jacobs <david@wit.io>
2012-03-25 21:59:02 -0700

    use incanter 1.3.0 instead of snapshot

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A study in machine learning, using Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
-                 [incanter "1.3.0-SNAPSHOT"]]
+                 [incanter "1.3.0"]]
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
   :test-paths ["spec"]
