07b8588a457471df63c4e72f3182755236476faa
David Jacobs <david@wit.io>
2012-04-05 00:51:40 -0700

    change local binding names to haskell style, round 2

--- spec/ml/regression/logistic_spec.clj
+++ spec/ml/regression/logistic_spec.clj
@@ -8,28 +8,28 @@
 (def data 
   (let [raw-data (read-data "data/ex2.1")
         m (first (dim raw-data))
-        x-first (vec (take m (repeat 1)))
-        x-rest (to-matrix (sel raw-data :cols [0 1]))
-        x (bind-columns x-first x-rest)
-        y (sel raw-data :cols 2)]
-    {:x x 
-     :y y
+        xs-first (vec (take m (repeat 1)))
+        xs-rest (to-matrix (sel raw-data :cols [0 1]))
+        xs (bind-columns xs-first xs-rest)
+        ys (sel raw-data :cols 2)]
+    {:xs xs 
+     :ys ys
      :m m
      :theta (matrix [[0] [0] [0]]) 
      :iterations 100
      :alpha 0.01}))
 
-(def expected-x
+(def expected-xs
   (matrix [[1.0 34.6237 78.0247]
            [1.0 30.2867 43.8950]
            [1.0 35.8474 72.9022]
            [1.0 60.1826 86.3086]]))
 
-(def expected-y [0 0 0 1])
+(def expected-ys [0 0 0 1])
 
 (deftest data-import
   (testing "data is read properly"
-    (let [{:keys [x y m]} data]
+    (let [{:keys [xs ys m]} data]
       (is (= m 100))
-      (is (matrices-equal? (take 4 x) expected-x))
-      (is (matrices-equal? (take 4 y) expected-y)))))
+      (is (matrices-equal? (take 4 xs) expected-xs))
+      (is (matrices-equal? (take 4 ys) expected-ys)))))
