2828b0db228089c9943772e2b6d4e06b83831c25
David Jacobs <david@wit.io>
2012-04-19 22:13:13 -0700

    add k nearest neighbors algorithm

---
+++ spec/ml/classification/knn_spec.clj
@@ -0,0 +1,20 @@
+(ns ml.classification.knn-spec
+  (:require [ml.classification.knn :as knn])
+  (:use ml.helpers
+        incanter.core
+        clojure.test
+        [clojure.math.numeric-tower :only (round)]))
+
+(def data 
+  (let [raw-data (read-data "data/knn")]
+    {:xs (to-matrix (sel raw-data :cols [0 1]))
+     :ys (sel raw-data :cols 2)}))
+
+(let [{:keys [xs ys]} data]
+  (deftest predict-nearest-neighbor
+    (testing "returns 1 if closer to points labeled 1"
+      (let [classification (knn/classify xs ys [0.2 0.5] 3)]
+        (is (= classification 1.0))))
+    (testing "returns 0 if closer to points labeled 0"
+      (let [classification (knn/classify xs ys [1.2 1.5] 3)]
+        (is (= classification 0.0))))))
---
+++ src/ml/classification/knn.clj
@@ -0,0 +1,22 @@
+(ns ml.classification.knn
+  (:use incanter.core
+        incanter.io
+        incanter.stats))
+
+(defn classify [xs ys point k]
+  (let [m (first (dim xs))
+        points (matrix (take m (repeat point)))]
+    (->> points
+      (minus xs)
+      (sq)
+      (map sum)
+      (map sqrt)
+      (bind-columns ys)
+      (to-vect)
+      (sort-by last)
+      (take k)
+      (map first)
+      (partition-by identity)
+      (map (juxt first count))
+      (max-key last)
+      (ffirst))))
