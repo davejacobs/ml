a7676c0aeeb244f404a904b0f03165953373a76f
David Jacobs <david@wit.io>
2012-08-21 16:12:07 -0700

    clean up cost function for neural network

--- src/ml/neural.clj
+++ src/ml/neural.clj
@@ -15,21 +15,21 @@
     (matrix-map g weighted)))
 
 (defn h [as theta-layers]
-  (reduce activate as theta-layers))
+  (matrix (reduce activate as theta-layers)))
 
 (defn+opts predict-category [as theta-layers | {categories (range)}]
   (let [category-for-max-index (comp (partial nth categories) index-of-max)
         probabilities (h as theta-layers)]
     (map category-for-max-index probabilities)))
 
-(defn logarithmic-cost [xs ys theta-layers]
-  (let [m (count xs)
+(defn logarithmic-cost [hypothesis actual]
+  (let [m (count hypothesis)
         multiplier (/ 1 m)
-        hypothesis (matrix (h xs theta-layers))
-        if-0-fn (mult (minus ys) (log hypothesis))
-        if-1-fn (mult (minus 1 ys) (log (minus 1 hypothesis)))
+        if-0-fn (mult (minus actual) (log hypothesis))
+        if-1-fn (mult (minus 1 actual) (log (minus 1 hypothesis)))
         sum-differences (minus if-0-fn if-1-fn)]
     (map sum sum-differences)))
 
 (defn+opts cost [xs ys theta-layers | {cost-fn logarithmic-cost}]
-  (cost-fn xs ys theta-layers))
+  (let [hypothesis (h xs theta-layers)]
+    (cost-fn hypothesis ys)))
